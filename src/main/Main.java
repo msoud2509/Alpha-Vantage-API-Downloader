6eb0906666eca3bd25474fe948e4d86b4891ffcb
msoud2509 <msouders06@gmail.com>
2024-12-24 17:59:20 -0600

    add controller factory for page navigation

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,20 +5,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import main.controllers.HomePageController;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("resources/HomePage.fxml"));
+            // move stage to hompage controller
+            loader.setControllerFactory(controllerClass -> new HomePageController(primaryStage));
             VBox root = loader.load();
 
             Scene scene = new Scene(root, 600, 400);
             primaryStage.setTitle("JavaFX Application");
             primaryStage.setScene(scene);
 
-            // Show the Stage
             primaryStage.show();
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/controllers/HomePageController.java
+++ src/main/controllers/HomePageController.java
@@ -1,21 +1,42 @@
 package main.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 
 import java.awt.Desktop;
+import java.io.IOException;
 import java.net.URI;
 
 public class HomePageController {
+    private Stage primaryStage;
+
+    public HomePageController(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
 
     @FXML
     private Button getStarted;
 
     @FXML
     private void handleGetStarted() {
-        System.out.println("Button clicked!");
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../resources/DownloadPage.fxml"));
+            Parent root = loader.load();
+
+            Scene newScene = new Scene(root);
+            primaryStage.setScene(newScene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @FXML
