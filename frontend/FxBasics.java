32a8f7c8c6169b6168c84572981fffc50ee63287
msoud2509 <msouders06@gmail.com>
2024-12-23 21:45:09 -0600

    add first frontend file

---
+++ frontend/FxBasics.java
@@ -0,0 +1,21 @@
+package frontend;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class FxBasics extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        StackPane layout = new StackPane();
+        Scene scene = new Scene(layout, 300, 300);
+        stage.setTitle("Fx start");
+        stage.show();
+    }
+}
