b1f5595598e20bffbd71e8b7347ad738607355d4
msoud2509 <msouders06@gmail.com>
2024-12-24 18:06:36 -0600

    merge frontend with main

---
+++ src/main/controllers/DownloadPageController.java
@@ -0,0 +1,18 @@
+package main.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class DownloadPageController {
+    private String api_key = "marc";
+    private String symbol = "harv";
+    private String function = "lose";
+
+    @FXML
+    private Button downloadButton;
+
+    private void handleDownload() {
+        ;
+    }
+
+}
